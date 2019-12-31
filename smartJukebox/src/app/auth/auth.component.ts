86f2770b13cc981b5b4bc2fd4b1fe2a2df09e968
arthurherman <arthurherman40@gmail.com>
2019-12-31 16:03:57 +0100

    Gestion modification url

--- smartJukebox/src/app/auth/auth.component.ts
+++ smartJukebox/src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { 
+    if(localStorage.getItem('userName') !== null){
+      router.navigate(['/home']);
+    }
+  }
 
   ngOnInit() {
   }
--- smartJukebox/src/app/channel/channel.component.ts
+++ smartJukebox/src/app/channel/channel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-channel',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChannelComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { 
+    if(localStorage.getItem('userName') === null){
+      router.navigate(['/auth']);
+    }
+  }
 
   ngOnInit() {
   }
